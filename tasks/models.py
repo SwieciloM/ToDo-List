e24abff49f001b202a186e780f5620e1e1c6cc6b
Michał Święciło <michal_00-00@o2.pl>
2024-12-17 15:02:42 +0100

    Task model created

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # null and blank are temporary True
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        # Orders tasks by completion status (False first, then True)
+        ordering = ['is_completed']
+
+    def clean(self):
+        # Ensure due_date is not before creation_date
+        if self.due_date and self.due_date < self.creation_date:
+            raise ValidationError("Due date cannot be earlier than creation date.")
+        super().clean()
+
+    def __str__(self):
+        # Return task title as string representation
+        return self.title
+     
+
