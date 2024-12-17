acd762ae7eeee31dac400c6008d261d6774e1622
Michał Święciło <michal_00-00@o2.pl>
2024-12-17 19:17:52 +0100

    Simple class based view for 'tasks' added

--- tasks/models.py
+++ tasks/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+def validate_due_date(date):
+    # Ensure due_date is not before creation_date
+    if date and date < timezone.now():
+        raise ValidationError('Due date cannot be in the past.')
 
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # null and blank are temporary True
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
-    is_completed = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)  # TODO: hide it from user during creation of task 
     creation_date = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateTimeField(null=True, blank=True)
+    due_date = models.DateTimeField(null=True, blank=True, validators=[validate_due_date])
 
     class Meta:
         # Orders tasks by completion status (False first, then True)
         ordering = ['is_completed']
-
-    def clean(self):
-        # Ensure due_date is not before creation_date
-        if self.due_date and self.due_date < self.creation_date:
-            raise ValidationError("Due date cannot be earlier than creation date.")
-        super().clean()
 
     def __str__(self):
         # Return task title as string representation
