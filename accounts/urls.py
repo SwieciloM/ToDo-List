27a3e881fd05dfaf1a18b2406e01bf198b0af6df
Michał Święciło <michal_00-00@o2.pl>
2024-12-18 18:57:42 +0100

    basic account_app logic added

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import CustomLoginView, HomeView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'accounts/home.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('my-tasks/')
+        return super().get(request, *args, **kwargs)
+
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class CustomLogoutView():
+    pass
+
+class CustomRegisterView():
+    pass
