aceff95ee141123c74fe03ae5ad51998a62705b2
Michał Święciło <michal_00-00@o2.pl>
2024-12-18 19:48:47 +0100

    home view replaced by login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomLoginView, HomeView
+from .views import CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
