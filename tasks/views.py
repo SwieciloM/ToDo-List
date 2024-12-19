d9d332084fab8d9d1fc6db1c03235f9cb13d0198
Michał Święciło <michal_00-00@o2.pl>
2024-12-19 13:35:30 +0100

    ensure each user can sea only his/her tasks

--- tasks/views.py
+++ tasks/views.py
@@ -11,6 +11,12 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(is_completed=False).count()
+        return context
+
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
