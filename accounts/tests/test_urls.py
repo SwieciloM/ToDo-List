68fc63a6c6610874584c0e9018401fcb33b0a63b
Michał Święciło <michal_00-00@o2.pl>
2025-01-01 14:50:57 +0100

    Urls tests added

--- accounts/tests/test_urls.py
+++ accounts/tests/test_urls.py
@@ -1,1 +1,32 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from accounts.views import CustomLoginView, RegisterView
+from django.contrib.auth.views import LogoutView
+
+
+class TestUrls(SimpleTestCase):
+    def test_login_url_resolves(self):
+        url = reverse('login')
+        self.assertEqual(resolve(url).func.view_class, CustomLoginView)
+
+    def test_logout_url_resolves(self):
+        url = reverse('logout')
+        self.assertEqual(resolve(url).func.view_class, LogoutView)
+
+    def test_register_url_resolves(self):
+        url = reverse('register')
+        self.assertEqual(resolve(url).func.view_class, RegisterView)
+
+
+class TestUrlStatusCodes(SimpleTestCase):
+    def test_login_page_status_code(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout_page_redirects(self):
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.status_code, 302)  # Redirect
+
+    def test_register_page_status_code(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
